53d57bf37d6dd21c2e04cef883e15e20d701f515
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2022-12-09 04:53:56 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,295 @@
+let arr= ["Are you a parking ticket? Cause you've got fine written all over you."
+,
+
+"Are you from Tennessee? Because you're the only 10 I see."
+
+,
+
+"Are you sure you’re not tired? You’ve been running through my mind all day."
+,
+
+"I’m not a photographer, but I can picture me and you together."
+
+,
+
+"I’m lost. Can you give me directions to your heart?"
+
+,
+
+"Did it hurt? When you fell from heaven?"
+
+,
+
+"If I could rearrange the alphabet, I’d put “U” and “I” together."
+
+
+,
+"Well, here I am. What are your other two wishes?"
+
+
+,
+"It’s a good thing I have my library card, because I am totally checking you out."
+
+
+,
+"They say Disney World is the happiest place on Earth, but clearly they've never stood next to you."
+
+
+,
+"Wanna touch my shirt? It's made of boyfriend/girlfriend/partner material."
+
+,
+
+"Are you Siri? Because you autocomplete me."
+
+,
+
+"If you and I were socks, we'd make a great pair."
+
+,
+
+"Do you know what the Little Mermaid and I have in common? We both want to be part of your world."
+
+
+,
+"You remind me of a magnet because you sure are attracting me over here."
+
+,
+
+"Are your parents bakers? Because you're a cutie pie."
+
+,
+
+"Are you a loan? Because you've got my interest."
+
+,
+
+"Is your dad a boxer? Because you’re a knockout!"
+
+
+,
+"If you were a vegetable, you’d be a cute-cumber."
+
+,
+
+"Are you an artist? Because you’re really good at drawing me in."
+
+,
+
+"I believe in following my dreams. Can I have your Instagram?"
+
+,
+
+"If you were a fruit, you’d be a fine-apple."
+
+,
+
+"If you were a flower, you'd be a daaaaaamn-delion."
+
+,
+
+"Aside from being sexy, what do you do for a living?"
+
+,
+
+"Are you a camera? Because every time I look at you, I smile."
+
+,
+
+"Do you have a name? Or can I call you mine?"
+
+,
+
+"I hope you know CPR because you are taking my breath away."
+
+,
+"Are you a time traveler? Because I see you in my future."
+,
+
+"If you were words on a page you'd be the fine print."
+
+,
+
+"You must be a magician. Because any time I look at you, everyone else disappears."
+
+,
+"Your hand looks heavy. Can I hold it for you?"
+
+,
+"Your lips look lonely. Would they like to meet mine?"
+
+,
+
+"I’m not currently an organ donor, but I’d love to give you my heart."
+
+,
+
+"You can delete the app now, I’m here."
+
+,
+"I'd take you to the movies, but they don't let you bring in your own snacks."
+
+,
+"Hey, how was heaven when you left it?"
+
+,
+
+"I have a phone number, you have a phone number—think of the possibilities."
+,
+
+
+"I bet you dinner that you won’t give me your number."
+
+,
+
+"How can I plan our wedding without having your number?"
+
+,
+"What number should I send my good morning texts to?"
+
+,
+
+"I’m writing a phone book, can I get your number?"
+
+,
+"Can I borrow your phone? I need to call God and tell him I’ve found his missing angel."
+
+,
+"My phone’s broken, it doesn’t have your number in it."
+,
+
+
+"I’m no mathematician, but I’m pretty good with numbers. Tell you what, give me yours and watch what I can do with it."
+
+,
+
+"I think there’s something wrong with my phone. Could you call it and see if it works?"
+
+,
+
+"When I text you goodnight later, what phone number should I use?"
+
+,
+
+"I bet my number sounds nicer than yours. Wanna hear it?"
+
+,
+
+"I think your number will be safer in my phone than in your head."
+
+,
+
+"I seem to have lost my phone number. Can I have yours?"
+
+,
+
+
+"I thought this was a dating app, but it must be a museum because you are a work of art."
+
+,
+
+"My mom told me not to talk to strangers online, but I’ll make an exception for you."
+
+
+
+,
+"What do you call a string of people lifting a mozzarella cheese? A cheesy pickup line."
+
+,
+
+"I think I saw you on Spotify, you were listed as the hottest single."
+
+
+,
+"You’re so fine, you made me forget my pickup line."
+
+,
+"Did we go to school together? I swear we had chemistry."
+
+,
+
+"Do you have a sunburn or are you always this hot?"
+
+
+,
+"I think you might be lacking some Vitamin Me."
+,
+
+
+"Are you a keyboard? Because you might just be my type."
+
+,
+"We’re not socks but I think we’d make a great pair."
+
+,
+"If being beautiful was a crime, you’d be guilty as charged."
+
+,
+
+
+"Can I show your profile to my friends to prove that angels really do exist?"
+
+
+,
+"Just wondering...if you're here, who's running heaven?"
+
+,
+
+"Do you like Star Wars? Because Yoda only one for me!"
+,
+
+
+
+
+"And finally, pickup lines that'll seal the deal:"
+
+,
+"Is your name Google? Because you’re everything I’ve been searching for."
+
+,
+
+"Want a raisin? No? Well, how about a date?"
+,
+
+
+"I always thought happiness started with an “H” but it looks like it starts with “U”."
+,
+
+
+"Now that I’ve seen you, life without you is like a broken pencil…pointless."
+,
+
+
+"Guess what I’m wearing? The smile you gave me."
+,
+
+
+"I think we’ve met before. Actually, never mind—I think it was just in my dreams."
+
+,
+
+"Do you play soccer? You look like a keeper."
+
+
+,
+"What’s your favorite drink? I’m asking so I know what to buy you when we go on our first date."
+
+
+,
+"Did you do something to my eyes? Because I can’t keep them off you."
+
+
+,
+"Did the sun come out, or did you just smile at me?"
+
+
+,
+"I'm pretty good at algebra...I can replace your X and you wouldn't need to figure out Y."
+
+
+,
+"Have we met? You look exactly like my next partner."
+]
+// console.log(arr);
+let ans=Math.floor(Math.random()*arr.length);
+document.write(arr[ans]);
